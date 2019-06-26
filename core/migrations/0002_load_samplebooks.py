17f1eeeaf9991a05fb6048e513ce1922781ca6cf
Michael <phamm89@gmail.com>
2019-06-26 14:49:35 -0400

    Created Migration for Loading Samplebooks

--- core/migrations/0002_load_samplebooks.py
+++ core/migrations/0002_load_samplebooks.py
@@ -20,27 +20,45 @@ def load_books(apps, schema_editor):
 
     with open(filename) as file:
         # Read CSV
-        reader = csv.DictReader(csv_file)
+        reader = csv.DictReader(file)
 
         # Create for loop to read through data
         for row in reader:
                 # Retrieve author or create one if necessary
-                author, _ = BookAuthor.objects.get_or_create(name=row['author'])
-                author.save()
+                book_author, created = BookAuthor.objects.get_or_create(name=row['author'])
+                book_author.save()
 
                 # Retrieve category or create one if necessary
-                category, _ = Category.objects.get_or_create(name=row['category'])
-                category.save()
+                book_category, created = Category.objects.get_or_create(name=row['category'])
+                book_category.save()
 
                 # Retrieve book fields
                 book = Book(
                         book_title = row['title'],
-                        book_author = author,
+                        book_author = book_author,
                         book_url = row['url'],
                         book_description = row['description'],
-                        book_category = category,
-                        db_date_added = 
-                        
-
+                        book_category = book_category,
+                        db_date_added = datetime.datetime.now()
                 )
+                book.save()
 
+
+# Migrations are not reversible; create function to reverse migration
+def reverse_load(apps, schema_editor):
+    Book = apps.get_model('core', 'Book')
+    BookAuthor = apps.get_model('core', 'BookAuthor')
+    Category = apps.get_model('core', 'Category')
+
+    Category.objects.all().delete()
+    BookAuthor.objects.all().delete()
+    Book.objects.all().delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('core', '0001_initial.'),
+    ]
+
+    operations = [migrations.RunPython(load_books, reverse_load)]
