ec8735976c5280812dfb0af34a56ae47f4642160
Michael <phamm89@gmail.com>
2019-06-25 20:58:48 -0400

    Added Category Model

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.urls import reverse # Used to generate URLs by reversing the URL patterns
 
-# Create your models here.
+
+# Created Models
+class Category(models.Model):
+    """Model representing a book category."""
+    book_category = models.CharField(max_length=200, help_text='Enter a book category (e.g. CSS, HTML, Javascript, Python, R)')
+    
+    class Meta:
+        verbose_plural_name = "categories"
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return self.book_category
+    
+    def get_absolute_url(self):
+        """Returns the url to access a detail record for a book category."""
+        return reverse('category-detail', args=[str(self.id)])
