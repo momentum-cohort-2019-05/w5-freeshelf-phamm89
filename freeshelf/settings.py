3c720c0e0df0f33cc218f7b7e79a1c7d3a55f72f
Michael <phamm89@gmail.com>
2019-06-27 15:22:52 -0400

    Changed base_generic to base

--- freeshelf/settings.py
+++ freeshelf/settings.py
@@ -23,7 +23,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'o3c&$#yz7^)e4pgy=(7(8#leq-5b_q2e6aquw@1256$%ss*p@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+in_production = bool(os.getenv('PRODUCTION'))
+DEBUG = not in_production
 
 ALLOWED_HOSTS = []
 
